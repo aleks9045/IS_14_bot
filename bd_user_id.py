1e5a53db1d9420547964df846c96b30985c5b360
aleks <aleksey9045@gmail.com>
2022-11-22 22:01:59 +0300

    ver. 0.2 добавление папки для баз данных

--- bd_user_id.py
+++ bd_user_id.py
@@ -2,7 +2,7 @@ import sqlite3
 
 
 def check_user_id(user_id):
-    connection = sqlite3.connect('users.bd')
+    connection = sqlite3.connect('Databases/users.bd')
     cur = connection.cursor()
     cur.execute("SELECT * FROM user;")
     one_result = cur.fetchone()
