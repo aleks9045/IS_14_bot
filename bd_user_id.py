ba0cc158cd302a962b09b737f780908b0055d550
aleks <aleksey9045@gmail.com>
2022-11-22 22:00:22 +0300

    ver. 0.2 добавление проверки на редактирование

--- bd_user_id.py
+++ bd_user_id.py
@@ -4,12 +4,6 @@ import sqlite3
 def check_user_id(user_id):
     connection = sqlite3.connect('users.bd')
     cur = connection.cursor()
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS user(
-        user_id1 INT, user_id2 INT, user_id3 INT, user_id4 INT);""")
-    connection.commit()
-    cur.execute("""INSERT INTO user VALUES(1205630682, 785663533, 1030737087, 1275232971)""")
-    connection.commit()
     cur.execute("SELECT * FROM user;")
     one_result = cur.fetchone()
     if user_id in one_result:
